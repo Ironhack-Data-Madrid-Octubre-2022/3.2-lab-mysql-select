5467f3bbb8d269ff7e83c1e4d76eba1b6d248e0a
Jaimemanrique <jaimemanrique6@gmail.com>
2022-11-01 16:18:00 +0100

    fin

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,4 +1,4 @@
-CHALLENGE 1
+#CHALLENGE 1
 
 select authors.au_id, au_lname, au_fname, title, pub_name 
 from authors
@@ -9,7 +9,7 @@ on titleauthor.title_id = titles.title_id
 inner join publishers
 on titles.pub_id = publishers.pub_id;
 
-CHALLENGE 2
+#CHALLENGE 2
 
 select authors.au_id, au_lname, au_fname, pub_name, count(title) from authors
 left join titleauthor
@@ -21,7 +21,7 @@ on titles.pub_id = publishers.pub_id
 group by pub_name, authors.au_id 
 order by count(title) desc;
 
-CHALLENGE 3
+#CHALLENGE 3
 
 select authors.au_id, au_lname, au_fname, sum(qty) as total
 from authors
@@ -35,7 +35,7 @@ group by authors.au_id
 order by total desc
 limit 3;
 
-CHALLENGE 4;
+#CHALLENGE 4;
 
 select authors.au_id, au_lname, au_fname, ifnull(sum(qty),0) as total
 from authors
