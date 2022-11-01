811ad4281a36d85606d957baa3653ab919f214b1
fjgarciaes <fj.garciaesurjc@gmail.com>
2022-11-01 20:22:49 +0100

    fin

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -23,3 +23,26 @@ on ta.title_id = t.title_id
 inner join publishers as p
 on t.pub_id = p.pub_id
 group by p.pub_name, a.au_id;
+
+CHALLENGE 3:
+
+from authors as a
+left join titleauthor as ta
+on ta.au_id = a.au_id
+left join titles as t
+on ta.title_id = t.title_id
+inner join sales as s
+on ta.title_id = s.title_id
+group by a.au_id
+order by quantity desc limit 3;
+
+CHALLENGE 4:
+
+select a.au_id as 'ID', a.au_lname as 'LAST NAME', a.au_fname as 'FIRT NAME', coalesce(sum(s.qty),0) as 'TOTAL'
+from authors as a
+left join titleauthor as ta
+on a.au_id = ta.au_id
+left join sales as s
+on s.title_id = ta.title_id
+group by a.au_id
+order by TOTAL desc
